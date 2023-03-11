9394b051be277bfce078c8bbaf3928e92d869943
Luis Florez <braian1k3@gmail.com>
2023-03-10 19:30:14 -0500

    Subir proyecto para validar fechas que incluso cuenta con una interfaz gráfica.

---
+++ Repositorio/ClaseInicial/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClaseInicial.Logic;
+
+try
+{
+    var dato1 = new Date();
+    var dato2 = new Date()
+    {
+        Year = 2025,
+        Month = 2,
+        Day = 19
+    };
+    var dato3 = new Date(year: 2019, month: 11, day:10); 
+
+    Console.WriteLine($"Primer fecha: {dato1}");
+    Console.WriteLine($"Segunda fecha: {dato2}");
+    Console.WriteLine($"Tercer fecha: {dato3}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
