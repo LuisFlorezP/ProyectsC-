10fb3be5c92ec39d5e387d078fc396bf5e729f93
Luis Florez <braian1k3@gmail.com>
2023-03-10 19:34:12 -0500

    Subir proyecto para practicar temas de matrices y su funcionamiento.

---
+++ Matrix/Matrix/Matrix.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Matrix
+    {
+        private int _n;
+        private int[,] _matrix;
+
+        public Matrix(int n)
+        {
+            _n= n;
+            _matrix = new int[_n, _n];
+        }
+
+        public void Fill() 
+        {
+            var random = new Random();
+
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    _matrix[i, j] = random.Next(1, 100);
+                }
+            }
+        }
+
+        public int Menu()
+        {
+            bool isValid = false;
+            int menu;
+            do
+            {
+                var validate = Console.ReadLine();
+                if (!int.TryParse(validate, out menu))
+                {
+                    Console.WriteLine("\nOption is not valid.");
+                    Console.Write("Try again: ");
+                    isValid = false;
+                }
+                else
+                {
+                    isValid = true;
+                }
+            } while (!isValid);
+
+            return menu;
+        }
+
+        public string GetMainDiagonal()
+        {
+            string mensaje = string.Empty;
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    if (i == j)
+                    {
+                        mensaje += _matrix[i, j] + "\t";
+                    }
+                    else
+                    {
+                        mensaje += "\t";
+                    }
+                }
+                mensaje += "\n";
+            }
+
+            return mensaje;
+        }
+
+        public string GetSecondaryDiagonal()
+        {
+            string mensaje = string.Empty;
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {   
+                    if (j == _n - i - 1)
+                    {
+                        mensaje += _matrix[i, j] + "\t";
+                    }
+                    else
+                    {
+                        mensaje += "\t";
+                    }
+                }
+                mensaje += "\n";
+            }
+
+            return mensaje;
+        }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < _n; i++)
+            {
+                for (int j = 0; j < _n; j++)
+                {
+                    output += $"{_matrix[i, j]} \t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+    }
+}
---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu;
+            Matrix matrix = new Matrix(10);
+            matrix.Fill();
+
+            do
+            {
+                Console.WriteLine(
+                    "\nMENU ARRAY.\n" +
+                        "   - Option 1: Change size matrix.\n" +
+                            "   - Option 2: Show matrix.\n" +
+                                "   - Option 3: Main diagonal.\n" +
+                                    "   - Option 4: Secondary diagonal.\n" +
+                                        "   - Option 0: Salir.\n");
+
+                Console.Write("Type your option: ");
+                menu = matrix.Menu();
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.Write("Enter your new size: ");
+                        matrix = new Matrix(matrix.Menu());
+                        matrix.Fill();
+                        Console.WriteLine("Successfully assigned!");
+                        break;
+                    case 2:
+                        Console.WriteLine("********************************* MATRIX *********************************\n" + matrix);
+                        break;
+                    case 3:
+                        Console.WriteLine(matrix.GetMainDiagonal());
+                        break;
+                    case 4:
+                        Console.WriteLine(matrix.GetSecondaryDiagonal()); break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("\nOption is not valid, just use numbers.");
+                        break;
+                }
+            } while (menu != 0);
+        }
+    }
+}
